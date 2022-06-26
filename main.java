025e3438b7343a23ff506e8f98278eec59611408
saghe7 <106884348+saghe7@users.noreply.github.com>
2022-06-25 21:26:32 -0300

    Update main.java
    agrego sistema que agrega los puntajes a un archivo de texto y después muestra todos los puntajes y nombres de los jugadores que han jugado

--- main.java
+++ main.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
 
 public class main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("JUEGO DE PREGUNTAS");
-		System.out.println("Se le hara una pregunta, conteste con la letra correcta");
 		String nombre;
-		System.out.println("Ingrse su nombre");
+		//Nombre de Usuario
+		System.out.println("Ingrse su nombre: ");
 		nombre = input.next();
 		int puntaje = 0;
 		
 		
 		System.out.println("Buena suerte " + nombre + "!");
 		System.out.println("Recuerda responder en mayusculas con la letra que corresponda");
+		
+		//Primera pregunta
 		System.out.println("Primera pregunta, Cuando termino la Segunda Guerra Mundial?");
 		String res1;
 		System.out.println("A.1945");
@@ -22,7 +28,7 @@ public class main {
 		System.out.println("D.1948");
 		res1 = input.next();
 		
-		if(res1.endsWith("A")) {
+		if(res1.equalsIgnoreCase("A")) {
 			puntaje += 10;
 			System.out.println("Bien hecho! Ganas 10 puntos, tienes " + puntaje + " puntos");
 			
@@ -33,6 +39,8 @@ public class main {
 			
 		}
 		
+		
+		//Segunda pregunta
 		System.out.println("Segunda pregunta, Cuantos metros mide el Monte Everest?");
 		String res2;
 		System.out.println("A.6,587 metros");
@@ -40,9 +48,9 @@ public class main {
 		System.out.println("C.7,456 metros");
 		System.out.println("D.9,945 metros");
 		res2 = input.next();
-		if(res2.endsWith("B")) {
+		if(res2.equalsIgnoreCase("B")) {
 			puntaje += 10;
-			System.out.print("Bien hecho! Ganas 10 puntos, tienes "+ puntaje + " puntos");
+			System.out.println("Bien hecho! Ganas 10 puntos, tienes "+ puntaje + " puntos");
 			
 		}
 		else {
@@ -52,6 +60,7 @@ public class main {
 		}
 		
 		
+		//Tercera pregunta
 		System.out.println("Tercera pregunta, Cual es el pais mas densidad poblacional del mundo?");
 		String res3;
 		System.out.println("A.China");
@@ -59,9 +68,9 @@ public class main {
 		System.out.println("C.Singapur");
 		System.out.println("D.India");
 		res3 = input.next();
-		if(res3.endsWith("C")) {
+		if(res3.equalsIgnoreCase("C")) {
 			puntaje += 10;
-			System.out.print("Bien hecho! Ganas 10 puntos, tienes "+ puntaje + " puntos");
+			System.out.println("Bien hecho! Ganas 10 puntos, tienes "+ puntaje + " puntos");
 			
 		}
 		else {
@@ -71,6 +80,7 @@ public class main {
 		}
 		
 		
+		//Cuarta pregunta
 		System.out.println("Cuarta pregunta, Cuantos años tiene la persona mas vieja viva?");
 		String res4;
 		System.out.println("A.119");
@@ -78,9 +88,9 @@ public class main {
 		System.out.println("C.118");
 		System.out.println("D.110");
 		res4 = input.next();
-		if(res4.endsWith("C")) {
+		if(res4.equalsIgnoreCase("C")) {
 			puntaje += 10;
-			System.out.print("Bien hecho! Ganas 10 puntos, tienes "+ puntaje + " puntos");
+			System.out.println("Bien hecho! Ganas 10 puntos, tienes "+ puntaje + " puntos");
 			
 		}
 		else {
@@ -89,7 +99,28 @@ public class main {
 			
 		}
 		
-		System.out.println("Tu puntaje final es " + puntaje);
+		System.out.println("Tu puntaje final es " + puntaje + ", se agregara a la base de datos");
+		//se añade el puntaje a la "base de datos(archivo .txt)"
+		try {
+			FileWriter Write = new FileWriter("C:\\Users..."/*Ruta del archivo de puntajes*/, true/*para que no borre y escriba sobre lo que ya este escrito en el archivo*/);
+			Write.write("\n" + nombre + " " + puntaje);
+			Write.close();
+		} catch (Exception e) {
+			System.out.println("error al agregar el nombre a la base de datos");
+		}
+		//Se muestran los puntajes anteriores
+		try {
+			BufferedReader Reader = new BufferedReader(new FileReader("C:\\Users..."/*Ruta del archivo de puntajes*/));
+			System.out.println("Puntajes anteriores: \n");
+			String line;
+			while ((line = Reader.readLine()) != null) {
+				System.out.println(line);
+				
+			}
+			
+			Reader.close();
+		} catch (Exception e) {
+		}
 	}
 
 }
